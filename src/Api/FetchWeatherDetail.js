87c65a7270a54bb097526a5fdbd0fef07ab3d7f9
Bijoy <bijoy10dutta@gmail.com>
2021-01-03 20:33:07 +0530

    Key Token Removed

--- src/Api/FetchWeatherDetail.js
+++ src/Api/FetchWeatherDetail.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '744460b0550f510a63e53950ab7e9c87';
+const API_KEY = 'Paste your key here';
 
 export const FetchWeatherDetail = async (query) => {
     const { data } = await axios.get(URL, {
